cb62c67538712362a1fc0fa8cf7b0e1473c3955c
kinjal-2107 <kinjalpatel01797@gmail.com>
2025-05-25 20:27:36 +0530

    add footer

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,7 +1,49 @@
+import Image from "react-bootstrap/Image";
+import img from "../assets/Brand.png";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/esm/Col";
+import Container from "react-bootstrap/esm/Container";
+import "../page/style.css";
 const Footer = () => {
   return (
     <>
-      <div>Footer</div>
+      <div className="bg">
+        <div>
+          <Image src={img} width={400} height={100} className="logo m-4 " />
+        </div>
+        <Row
+          className="text-center text-md-start m-5"
+          style={{ color: "white" }}
+        >
+          <Col xs={12} md={4} lg={2} className="mb-4">
+            <h5>Main Menu</h5>
+            <ul className="list-unstyled">
+              <li href="/">Shop By Product</li>
+              <li>Collection</li>
+              <li>Track Order</li>
+              <li>Contact Information</li>
+            </ul>
+          </Col>
+          <Col xs={12} md={4} lg={2} className="mb-4">
+            <h5>Our Policy</h5>
+            <ul className="list-unstyled">
+              <li>Exchange Policy</li>
+              <li>Return Policy</li>
+              <li>Shipping Policy</li>
+              <li>Cancellation Policy</li>
+            </ul>
+          </Col>
+          <Col xs={12} md={4} lg={2} className="mb-4">
+            <h5>Support</h5>
+            <ul className="list-unstyled">
+              <li>Call Us</li>
+              <li>Chat With Us</li>
+              <li>Exchange and Replacement</li>
+              <li>Cancel Order</li>
+            </ul>
+          </Col>
+        </Row>
+      </div>
     </>
   );
 };
